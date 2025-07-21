55a3d70ef2199afb9ba73112028936064e0c59b7
zaawmilen <166408897+zaawmilen@users.noreply.github.com>
2025-07-21 15:23:23 +0300

    api

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import api  from '../utils/api';
 import {User} from '../types/task';
 
 export interface LoginProps {
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import axios from 'axios';
+import api  from '../utils/api';
 import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
